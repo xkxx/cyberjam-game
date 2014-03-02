774415abfca0877382a73f56b76abace397c22a0
xkx <xukexiang@gmail.com>
2014-03-01 16:32:42 -0800

    initial build system

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     uglify = require('gulp-uglify'),
-    browerify = require('gulp-browerify');
+    browerify = require('gulp-browerify'),
+    htmlreplace = require('gulp-html-replace');
 
 gulp.task('lint', function() {
     return gulp.src('js/*.js')
@@ -9,10 +10,16 @@ gulp.task('lint', function() {
         .pipe(jshint.reporter('default'));
 });
 
-var gulp.task('default'), function(){
+var gulp.task('bundle', function() {
     return gulp.src('js/app.js')
         .pipe(browserify())
         .pipe(uglify())
-        .pipe(gulp.dest('js/dist/js'));
+        .pipe(gulp.dest('dist/'));
 
+});
+
+var gulp.task('htmlreplace', function() {
+    return gulp.src('app.html')
+        .pipe(htmlreplace('prod', ['vue.min.js', 'quintus-all.min.js'])
+        .pipe(gulp.dest('dist/'));
 });
