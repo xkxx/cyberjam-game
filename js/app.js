45d894ea80e4da35c61115ac95f89bcb2c7073a4
xkx <xukexiang@gmail.com>
2014-03-02 15:19:22 -0800

    adds skeletons for dialogs

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var Q = Quintus()
+    .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI, Audio")
+    .setup('gameCanvas')
+    .controls(true).touch();
+
+Q.Sprite.extend("Player", {
+   init: function() {
+     this.add("2d, platformerControls");
+   }
+ });
---
+++ js/dialogs.js
@@ -0,0 +1,50 @@
+/*
+ This file contains the dialog list/tree logic
+
+ A dialog node is a standard JavaScript object.
+ The format is as follows:
+
+{
+    // a signal that triggers the dialog.
+    // can be the player clicking on a NPC, or a timed event
+    trigger: "",
+    // alternatively, if the node is a response candidate,
+    // triggerText will be displayed, and activate the dialog
+    // if selected
+    triggerText: "",
+    // the dialog content to be displayed
+    // Will display in multiple "slides" if is multiline
+    // Won't display anything if empty
+    content: "",
+    // signal to trigger when the dialog is active
+    emit: "",
+    // function to execute when the dialog is active.
+    ontrigger: function(dialog) {},
+    // list of reponse candidates to select from.
+    // each response is a new dialog node
+    responses: []
+}
+
+example:
+
+var NPC1 = {
+    trigger: "npc1-click",
+    message: "Hello. What's up?",
+    responses: [
+        {
+            triggerText: "Nothing. Bye",
+            content: "Bye!"
+        },
+        {
+            triggerText: "Lend me 10 bucks",
+            content: "Not again..."
+            ontrigger: function(dialog) {
+                Dialogs.remove(dialog); // removes dialog forever
+            }
+        }
+    ]
+}
+
+*/
+
+dialogs = [];
