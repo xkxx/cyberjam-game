09241cd29405f0e699ac8f3288938b0574fa368d
Connor Osborn <cdosborn@uw.edu>
2014-03-08 18:01:28 -0800

    fixed merge issue

--- js/scenes.js
+++ js/scenes.js
@@ -113,44 +113,78 @@ exports.scenes = function(Q) {
         stage.insert(entrance);
     });
 
-    Q.scene("kitchen", function(stage) {
-        stage.width = 700;
-        stage.height = 232;
-        stage.add("viewport");
-
-        var actionButton = new Q.UI.Button({
-            asset: 'action.png',
-            x: 0,
-            y: 100,
-            hidden: true
-        }, function() {
-            Q.npcNearby.click();
-        });
-        stage.actionButton = actionButton;
-
-        // bg
-        var bg = new Q.Sprite({
-            x: 0,
-            y: stage.height / 2,
-            h: stage.height,
-            w: stage.width,
-            asset: 'kitchen-scene.png',
-            type: 0 // !!important! You MUST specify Sprite type
-        });
-
-        var actionButton = new Q.UI.Button({
-            asset: 'action.png',
-            x: 0,
-            y: 100,
-            hidden: true
-        }, function() {
-            Q.npcNearby.click();
-        });
-        stage.actionButton = actionButton;
-        stage.insert(actionButton);
-
-        stage.add("viewport").follow(player, {x: true, y: false});
-    }, {sort: true});
+     Q.scene("kitchen", function(stage) {
+          stage.width = 700;
+          stage.height = 232;
+          stage.add("viewport");
+  
+          var actionButton = new Q.UI.Button({
+              asset: 'action.png',
+              x: 0,
+              y: 100,
+              hidden: true
+          }, function() {
+              Q.npcNearby.click();
+          });
+          stage.actionButton = actionButton;
+  
+          // bg
+          var bg = new Q.Sprite({
+              x: 0,
+              y: stage.height / 2,
+              h: stage.height,
+              w: stage.width,
+              asset: 'kitchen-scene.png',
+              type: 0 // !!important! You MUST specify Sprite type
+          });
+  
+          // back wall
+          var back_wall = new Q.Sprite({
+              x: 0,
+              y: 32,
+              h: 1,
+              w: stage.width,
+              type: C.SPRITE_BLOCKER
+          });
+  
+          // front wall
+          var front_wall = new Q.Sprite({
+              x: 0,
+              y: 200,
+              h: 1,
+              w: stage.width,
+              type: C.SPRITE_BLOCKER
+          });
+  
+          // entrance right
+          var entrance_left = new Q.Sprite({
+              x: stage.width / 2 - 112 / 2,
+              y: stage.height / 2,
+              w: 112,
+              h: 232, 
+              asset: "wall-entrance.png",
+          });
+  
+          // entrance right
+          var entrance_right = new Q.Sprite({
+              x: -stage.width / 2 + 112 / 2,
+              y: stage.height / 2,
+              w: 112,
+              h: 232, 
+              asset: "wall-entrance-flip.png",
+          });
+  
+          var player = new Q.Player(0, 116);
+  
+          stage.insert(actionButton);
+          stage.insert(bg);
+          stage.insert(back_wall);
+          stage.insert(front_wall);
+          stage.insert(player);
+          stage.insert(entrance_left);
+          stage.insert(entrance_right);
+     });
+ 
 
 //  Q.scene("sleeping-area", function(stage) {
 //      // back wall
